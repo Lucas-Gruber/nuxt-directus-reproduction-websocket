0cf746751349bc0c1d7ae0640e031e97b94fc13d
lucas-gruber <blaumc@icloud.com>
2024-06-21 14:10:58 +0200

    removed try-catch for nuxt-internal error page and added onWebSocket-event logs

--- app.vue
+++ app.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-const { connect } = useDirectusRealtime()
+const { connect, onWebSocket } = useDirectusRealtime()
 
-try {
-  await connect()
-} catch (e) {
-  console.log(e)
-}
+// try {
+await connect()
+
+onWebSocket('open', function () {
+  console.log({ event: 'onopen' })
+})
+
+onWebSocket('message', function (message) {
+  const { type, data } = message
+  console.log({ event: 'onmessage', data })
+})
+
+onWebSocket('close', function () {
+  console.log({ event: 'onclose' })
+})
+
+onWebSocket('error', function (error) {
+  console.log({ event: 'onerror', error })
+})
+// } catch (e) {
+//   console.log(e)
+// }
 </script>
 
 <template>
